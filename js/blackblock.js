a4c84585adab48ea69ab6b194820f6c1f674dedf
unknown <jaker.raimov@gmail.com>
2022-03-27 19:13:48 +0300

    Done

--- js/blackblock.js
+++ js/blackblock.js
@@ -20,3 +20,43 @@ $("header menu li").on("click", function (param){
     console.log($(this));
     $(this).children(".dropdown").slideToggle()
 })
+
+
+
+
+// General
+//Select box
+var selected = $(".select .selected");
+var option = $(".select .option");
+selected.click(function (e) {
+
+    if (isVisible($(this).next(".options")[0])) {
+        $(this).next(".options")[0].style.cssText = "visibility: hidden; opacity: 0; margin-top: 0px";
+        $(this).find("i.fa-chevron-down")[0].style.transform = "rotate(0deg)";
+    } else {
+        $(this).next(".options")[0].style.cssText = "visibility: visible; opacity: 1; margin-top: 10px";
+        $(this).find("i.fa-chevron-down")[0].style.transform = "rotate(180deg)";
+    }
+
+});
+
+option.click(function (e) {
+    $(this).closest(".select").find("label").html(this.innerHTML)
+    $(this).closest(".select").find("input").val($(this).data("value"));
+    $(this).closest(".options")[0].style.cssText = "visibility: hidden; opacity: 0; margin-top: 0px";
+
+    $(this).closest(".options").siblings(".selected").find("i.fa-chevron-down")[0].style.transform = "rotate(0deg)";
+});
+
+
+
+// Animation
+jQuery('.mycard').viewportChecker({
+    classToAdd: 'visible animate__animated animate__fadeInUp',
+    offset: 100
+});
+
+jQuery('.sitecard').viewportChecker({
+    classToAdd: 'visible animate__animated animate__fadeInUp',
+    offset: 100
+});
