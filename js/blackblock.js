edf37c5a0621a5bb3efd8047fa4a5ac2aabdca11
NordsKingArt <jaker.raimov@gmail.com>
2022-04-26 16:39:16 +0500

    cleared from vue

--- js/blackblock.js
+++ js/blackblock.js
@@ -1,90 +1,51 @@
-import SaveBtn from './components/SaveBtn.js'
-import Comment from './components/Comment.js'
+// Header
+$("header.header_transparent #search_btn, header.header_transparent .searchfield .close").click(() => {
+    $("header.header_transparent .searchfield").toggleApearAnimation("animate__fadeInDown", "animate__fadeOutUp", true)
+    $("header.header_transparent .searchfield input").focus()
+})
+
+// Menu Button
+$("header .menu-btn")[0].addEventListener("click", () => {
+    if ($("header .menu-btn").hasClass("active")) {
+        $("header .menu-btn").removeClass("active");
+        $("header menu")[0].style.cssText = "visibility: hidden; opacity: 0";
+    } else {
+        $("header .menu-btn").addClass("active");
+    
+        $("header menu")[0].style.cssText = "visibility: visible; opacity: 1";
+    }
+})
+
+$("header menu li").on("click", function (param){
+    console.log($(this));
+    $(this).children(".dropdown").slideToggle()
+})
 
 
 
+// General
+//Select box
+var selected = $(".select .selected");
+var option = $(".select .option");
+selected.click(function (e) {
 
-Vue.createApp({
-    data() {
-        return {
-            ...pageSpecific.data
-        }
-    },
-    mounted() {
-        pageSpecific.data.context = this
-        pageSpecific.mounted(this);
-        onMount();
-    },
-    methods: {
-        ...pageSpecific.methods
-    },
-    watch: {
-        ...pageSpecific.watch,
-        showSavePostDialog(old, _new){
-            setTimeout(() => {
-                reinit_popups()                
-            }, 10);
-        }
-    },
-    components: {
-        SaveBtn,
-        Comment
-    },
-    delimiters: ['${', '}']
-}).mount("#app")
+    if (isVisible($(this).next(".options")[0])) {
+        $(this).next(".options")[0].style.cssText = "visibility: hidden; opacity: 0; margin-top: 0px";
+        $(this).find("i.fa-chevron-down")[0].style.transform = "rotate(0deg)";
+    } else {
+        $(this).next(".options")[0].style.cssText = "visibility: visible; opacity: 1; margin-top: 10px";
+        $(this).find("i.fa-chevron-down")[0].style.transform = "rotate(180deg)";
+    }
 
+});
 
-function onMount(){
-        // Header
-        $("header.header_transparent #search_btn, header.header_transparent .searchfield .close").click(() => {
-            $("header.header_transparent .searchfield").toggleApearAnimation("animate__fadeInDown", "animate__fadeOutUp", true)
-            $("header.header_transparent .searchfield input").focus()
-        })
-        
-        // Menu Button
-        $("header .menu-btn")[0].addEventListener("click", () => {
-            if ($("header .menu-btn").hasClass("active")) {
-                $("header .menu-btn").removeClass("active");
-                $("header menu")[0].style.cssText = "visibility: hidden; opacity: 0";
-            } else {
-                $("header .menu-btn").addClass("active");
-            
-                $("header menu")[0].style.cssText = "visibility: visible; opacity: 1";
-            }
-        })
-        
-        $("header menu li").on("click", function (param){
-            console.log($(this));
-            $(this).children(".dropdown").slideToggle()
-        })
-        
-        
-        
-        
-        // General
-        //Select box
-        var selected = $(".select .selected");
-        var option = $(".select .option");
-        selected.click(function (e) {
-        
-            if (isVisible($(this).next(".options")[0])) {
-                $(this).next(".options")[0].style.cssText = "visibility: hidden; opacity: 0; margin-top: 0px";
-                $(this).find("i.fa-chevron-down")[0].style.transform = "rotate(0deg)";
-            } else {
-                $(this).next(".options")[0].style.cssText = "visibility: visible; opacity: 1; margin-top: 10px";
-                $(this).find("i.fa-chevron-down")[0].style.transform = "rotate(180deg)";
-            }
-        
-        });
-        
-        option.click(function (e) {
-            $(this).closest(".select").find("label").html(this.innerHTML)
-            $(this).closest(".select").find("input").val($(this).data("value"));
-            $(this).closest(".options")[0].style.cssText = "visibility: hidden; opacity: 0; margin-top: 0px";
-        
-            $(this).closest(".options").siblings(".selected").find("i.fa-chevron-down")[0].style.transform = "rotate(0deg)";
-        });
-}
+option.click(function (e) {
+    $(this).closest(".select").find("label").html(this.innerHTML)
+    $(this).closest(".select").find("input").val($(this).data("value"));
+    $(this).closest(".options")[0].style.cssText = "visibility: hidden; opacity: 0; margin-top: 0px";
+
+    $(this).closest(".options").siblings(".selected").find("i.fa-chevron-down")[0].style.transform = "rotate(0deg)";
+});
 
 
 function reinit_popups(){
--- js/blog.js
+++ js/blog.js
@@ -1,90 +1,25 @@
-var pageSpecific = {
-    data: {
-            videoWidth: 0,
-            videoHeight: 0,
-            videoTop: 0,
-            watchVideo: false, // This one used to toggle fullscreen behavior
-            showVideo: false, // This one used to show video on background or not
-            saved: false,
-            showSavePostDialog: false,
-            showDeleteCommentDialog: false,
+renderEditor("textarea[name='comment']", 'Let`s write some cool description!', 'comment'); // Rendering froala editor in comments section
 
-            
-            deleteCommentId: null, // Null by default, once trash icon on comment is clicked, this field is filled up with that comment's id
-            comments: [
-                {
-                    id: 1,
-                    avatar: "/img/skins/10.png",
-                    reply : false,
-                    authorized : true,
-                    username : "Leif Haraldson",
-                    role : 0,
-                    date : "July 15, 2017",
-                    comment : "First download the link then copy the shaders file and press windows key + r enter and go to minecraft go to shaders and paste the file"
-                },
-                {
-                    id: 2,
-                    avatar: "/img/skins/5.png",
-                    reply : true,
-                    authorized : true,
-                    username : "Erik",
-                    role : 0,
-                    date : "July 18, 2017",
-                    comment : "Vue defaults Boolean props to false if no default was specified in the prop declaration. The author of Vue explains the reasoning."
-                }
-            ],
 
-            context: null
-    },
-    mounted(context) {
-        renderEditor("textarea[name='comment']", 'Let`s write some cool description!', 'comment'); // Rendering froala editor in comments section
-
-        // Video stuff
-        {
-            window.addEventListener('resize', (e) => {
-                context.showVideo = window.innerWidth > 1200
-            });
-            setTimeout(() => {
-                if (window.innerWidth > 1200) context.showVideo = true
-            }, 5000);
-        }
-
-        // I will assume that the given YouTube video aspect ratio is 16:9
-        const ASRATIO = 1.77778
-        context.videoWidth = window.innerWidth;
-        context.videoHeight = (window.innerWidth/ASRATIO);
-        context.videoTop = (225-context.videoHeight/2)
-    },
-    methods: {
-        toggleFullScreen() {
-            let iframe = $("#player")
-            if(this.watchVideo){
-                player.mute()
-
-                iframe.width("inherit")
-                iframe.height("inherit")
-            }
-            else{
-                player.unMute()
-
-                iframe.width(this.videoWidth-(window.innerWidth*0.2)+"px")
-                iframe.height(iframe.width()/1.7778)
-            }
-            this.watchVideo = !this.watchVideo;
-        },
-        toggleSavePostDialog(){
-            this.showSavePostDialog = !this.showSavePostDialog
-
-            this.saved = true
-        },
-
-        onCommentDeleteClicked(id){
-            this.deleteCommentId = id
-            this.showDeleteCommentDialog = true;
-        },
-        deleteComment(){
-            // Send query to backend to delete comment
-            
-        }
-    },
+// Video stuff
+{
+    window.addEventListener('resize', (e) => {
+        if(window.innerWidth > 1200) $("#entry_image").fadeOut()
+    });
+    setTimeout(() => {
+        if (window.innerWidth > 1200) $("#entry_image").fadeOut()
+    }, 5000);
 }
+
+
+// I will assume that the given YouTube video aspect ratio is 16:9
+var videoContainer = $("#videoContainer");
+
+const ASRATIO = 1.77778
+let videoWidth = window.innerWidth;
+let videoHeight = (window.innerWidth/ASRATIO);
+let videoTop = (225-videoHeight/2)
+
+videoContainer.width(videoWidth+"px")
+videoContainer.height(videoHeight+"px")
+videoContainer.css("top",videoTop+"px")
